52c72ecf9f810186b4f30da7f416a0fa2dffd230
Jethro Beekman <jethro@jbeekman.nl>
2016-07-10 01:07:09 -0700

    Add script to generate limited documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! <p id="core_io-show-docblock"></p>
+//! This is just a listing of the functionality available in this crate. See
+//! the [std documentation](https://doc.rust-lang.org/nightly/std/io/index.html)
+//! for a full description of the functionality.
 #![allow(stable_features,unused_features)]
 #![feature(question_mark,const_fn,collections,alloc,unicode,copy_from_slice,str_char)]
 #![no_std]
