3b2fb2b87911169f3082702f66caf32e1ff37ecc
Jeffrey Seyfried <jeffrey@fortanix.com>
2017-04-04 17:48:59 -0700

    Update to 2017-04-04

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,41 @@
 //! the [std documentation](https://doc.rust-lang.org/nightly/std/io/index.html)
 //! for a full description of the functionality.
 #![allow(stable_features,unused_features)]
-#![feature(question_mark,const_fn,collections,alloc,unicode,copy_from_slice,str_char,try_from)]
+#![feature(question_mark,const_fn,collections,alloc,unicode,copy_from_slice,str_char,try_from,str_internals)]
 #![no_std]
 
 #[cfg_attr(feature="collections",macro_use)]
 #[cfg(feature="collections")] extern crate collections;
 #[cfg(feature="alloc")] extern crate alloc;
-extern crate rustc_unicode;
+extern crate std_unicode;
 
 #[cfg(not(feature="collections"))]
 pub type ErrorString = &'static str;
 
 // Provide Box::new wrapper
 #[cfg(not(feature="alloc"))]
-struct FakeBox<T>(core::marker::PhantomData<T>);
+#[derive(Debug)]
 #[cfg(not(feature="alloc"))]
+struct FakeBox<T>(T);
 impl<T> FakeBox<T> {
-	fn new(val: T) -> T {
-		val
+	fn new(val: T) -> Self {
+		FakeBox(val)
 	}
+}
+
+#[cfg(not(feature="alloc"))]
+impl<T> ::core::ops::Deref for FakeBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+#[cfg(not(feature="alloc"))]
+impl<T> ::core::ops::DerefMut for FakeBox<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
 }
 
 // Needed for older compilers, to ignore vec!/format! macros in tests
