fc7626957538f6719c9cb9767b2954774b794586
roblabla <unfiltered@roblab.la>
2018-03-06 11:36:24 +0100

    Add patch for latest version of compiler

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! the [std documentation](https://doc.rust-lang.org/nightly/std/io/index.html)
 //! for a full description of the functionality.
 #![allow(stable_features,unused_features)]
-#![feature(question_mark,const_fn,collections,alloc,unicode,copy_from_slice,str_char,try_from,str_internals)]
+#![feature(question_mark,const_fn,collections,alloc,unicode,copy_from_slice,str_char,try_from,str_internals,slice_internals)]
 #![no_std]
 
 #[cfg_attr(feature="collections",macro_use)]
