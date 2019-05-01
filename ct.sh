74ade3b08514dc07ebdf04642557e862e0359b71
Jethro Beekman <jethro@jbeekman.nl>
2019-04-30 18:27:50 -0700

    Parse mapping.rs manually, fix CI versions.
    Fixes #22

--- ct.sh
+++ ct.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -e
+
+# Old versions should come first so we go from oldest Cargo.lock version to
+# newest when building.
 RUST_VERSIONS=$(awk '{print $1}' <<EOF
-    nightly-2019-04-27 # core_io release
-    nightly-2018-08-14 # edge case: old features disallowed
-    nightly-2018-08-06 # edge case: old features allowed
-    nightly-2018-03-07 # core_io release
-    nightly-2018-01-02 # edge case: memchr in core
-    nightly-2018-01-01 # edge case: no memchr in core
-    nightly-2017-06-16 # edge case: no collections crate
-    nightly-2017-06-15 # edge case: collections crate
-    nightly-2017-04-09 # core_io release
-    nightly-2017-03-04 # edge case: std_unicode crate
-    nightly-2017-03-03 # edge case: rustc_unicode crate
-    nightly-2017-01-18 # edge case: rustc_unicode crate
-    nightly-2016-12-05 # edge case: no unicode crate
-    nightly-2016-10-28 # core_io release
+    nightly-2016-03-11 # first supported version
     nightly-2016-07-07 # core_io release
-    nightly-2015-05-01 # some old version
+    nightly-2016-10-28 # core_io release
+    nightly-2016-12-05 # edge case: no unicode crate
+    nightly-2017-01-18 # edge case: rustc_unicode crate
+    nightly-2017-03-03 # edge case: rustc_unicode crate
+    nightly-2017-03-04 # edge case: std_unicode crate
+    nightly-2017-04-09 # core_io release
+    nightly-2017-06-15 # edge case: collections crate
+    nightly-2017-06-16 # edge case: no collections crate
+    nightly-2018-01-01 # edge case: no memchr in core
+    nightly-2018-01-02 # edge case: memchr in core
+    nightly-2018-03-07 # core_io release
+    nightly-2018-08-06 # edge case: old features allowed
+    nightly-2018-08-14 # edge case: old features disallowed
+    nightly-2019-04-27 # core_io release
 EOF
 )
 
